310dc3cf19b7743a3432bc6379484ee1041dcc5d
Damien Royer <royerdamien57@gmail.com>
2026-03-25 10:02:35 +0100

    [Misc] Disable drop weapons after buy time

--- Misc/MiscPlugin.cs
+++ Misc/MiscPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Cvars;
 using CounterStrikeSharp.API.Modules.Utils;
 using Utils;
 
@@ -10,6 +11,22 @@ public sealed class MiscPlugin : BasePlugin
 {
     public override string ModuleName => "Misc";
     public override string ModuleVersion => "1.0.0";
+
+    [GameEventHandler]
+    public HookResult OnRoundStart(EventRoundStart evt, GameEventInfo info)
+    {
+        if (Utility.IsWarmup)
+        {
+            return HookResult.Continue;
+        }
+
+        var buyTime = ConVar.Find("mp_buytime")?.GetPrimitiveValue<float>() ?? 20;
+        AddTimer(buyTime, () => Server.ExecuteCommand("mp_death_drop_gun 0"));
+
+        Server.ExecuteCommand("mp_death_drop_gun 1");
+
+        return HookResult.Continue;
+    }
 
     [GameEventHandler]
     public HookResult OnPlayerDeath(EventPlayerDeath evt, GameEventInfo info)
